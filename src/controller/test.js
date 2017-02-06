3a0e3d958acaab0cf6ef826fa91da7eea54e68d9
yinzheng <yinzheng@pwrd.com>
2017-02-06 18:08:49 +0800

    test内容

---
+++ src/controller/test.js
@@ -0,0 +1,63 @@
+var xlsx = require('node-xlsx');
+var path = require('path');
+var lodash = require('lodash');
+var fs = require('fs');
+
+module.exports = {
+
+	run:function(res , params){
+		this.res = res;
+		var key = params[0];
+		if(!this[key]){return;}
+		this[key]();
+	},
+
+	vaild:function(){
+		this.res.send('HELLO WORLD !!!');
+	},
+
+	sourceData:function(){
+		var sourceData = this._getData(this._getSourceFile);
+		this.res.json(sourceData[0]);
+	},
+
+	setAgentData:function(){
+		var DBData = this._getData(this._getDBFile);
+		DBData[0] = {
+			name:'agent',
+			data:[
+				['name','mark']
+				['小宠',''],
+				['维斯康',''],
+				['卫仕',''],
+				['维克',''],
+				['爱迪森',''],
+			]
+		};
+		this._setData(this._getDBFile,DBData);
+		this.res.json(DBData);
+	},
+
+	_setData:function(fn,data){
+		var file = fn();
+		var obj = xlsx.build(data);
+		fs.writeFileSync(file,obj,'binary');
+	},
+
+	_getData:function(fn){
+		var file = fn();
+		var obj = xlsx.parse(file);
+		return obj;
+	},
+
+	_getDBFile:function(){
+		var file = path.resolve(__dirname + "/../../data/excel/DB.xlsx");
+		return file;
+	},
+
+	_getSourceFile:function(){
+		var file = path.resolve(__dirname + "/../../data/excel/宠物用品价格表.xlsx");
+		return file;
+	},
+
+}
--- src/core/route.js
+++ src/core/route.js
@@ -1,9 +1,36 @@
 var express = require('express');
+var path = require('path');
 
 module.exports = function(app){
 
+	app.use(express.static('public'));
+
 	app.get('/', function (req, res) {
-	   res.send('Hello World');
-	})
-	
+		// res.sendFile( __dirname + "/../html/" + "list.html" );
+		var file = path.resolve(__dirname + "/../html/" + "list.html")
+		res.sendFile(file);
+	});
+
+	app.get('/Test/*', function (req, res) {
+		var controller = require('../controller/test');
+		controller.run(res,req.params);
+	});
+
+	app.get('/Test1',function(req,res){
+		var log = JSON.stringify(req.path);
+		res.send(log);
+	});
+
+	app.get('/Test2',function(req,res){
+		res.json({ user: 'tobi' });
+	});
+
+	app.get('/Test3/*',function(req,res){
+		var path = req.path;
+		var baseUrl = req.baseUrl;
+		var originalUrl = req.originalUrl;
+		var params = req.params;
+		res.json({ user: 'tobi',path:path,baseUrl:baseUrl,originalUrl:originalUrl,params:params });
+	});
+
 };
